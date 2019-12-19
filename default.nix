2ab2ab4724d4dd504f806df284d6f99a5d8f3011
Nikolay Korotkiy <sikmir@gmail.com>
2019-12-19 09:46:51 +0300

    Add gpxsee-maps

--- default.nix
+++ default.nix
@@ -9,5 +9,6 @@ in
   datamaps = callPackage ./pkgs/datamaps { inherit (sources) datamaps; };
   gpx-layer = perlPackages.callPackage ./pkgs/gpx-layer { inherit (sources) gpx-layer; };
   gpxpy = python3Packages.callPackage ./pkgs/gpxpy { inherit (sources) gpxpy; };
+  gpxsee-maps = callPackage ./pkgs/gpxsee-maps { inherit (sources) GPXSee-maps; };
   mbutil = python3Packages.callPackage ./pkgs/mbutil { inherit (sources) mbutil; };
 }
