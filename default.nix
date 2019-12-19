89e0c221d0370ff9bc362411b0a6538b4948c946
Nikolay Korotkiy <sikmir@gmail.com>
2019-12-19 11:10:00 +0300

    Add gt4gd

--- default.nix
+++ default.nix
@@ -10,5 +10,6 @@ in
   gpx-layer = perlPackages.callPackage ./pkgs/gpx-layer { inherit (sources) gpx-layer; };
   gpxpy = python3Packages.callPackage ./pkgs/gpxpy { inherit (sources) gpxpy; };
   gpxsee-maps = callPackage ./pkgs/gpxsee-maps { inherit (sources) GPXSee-maps; };
+  gt4gd = python3Packages.callPackage ./pkgs/gt4gd { inherit (sources) google-translate-for-goldendict; };
   mbutil = python3Packages.callPackage ./pkgs/mbutil { inherit (sources) mbutil; };
 }
