7951f34e6761d9303a5da3ab7acf74e39a87cef3
Nikolay Korotkiy <sikmir@gmail.com>
2019-12-19 03:23:58 +0300

    Add gpxpy

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@ let
 in
 {
   datamaps = callPackage ./pkgs/datamaps { inherit (sources) datamaps; };
-  mbutil = python3Packages.callPackage ./pkgs/mbutil { inherit (sources) mbutil; };
   gpx-layer = perlPackages.callPackage ./pkgs/gpx-layer { inherit (sources) gpx-layer; };
+  gpxpy = python3Packages.callPackage ./pkgs/gpxpy { inherit (sources) gpxpy; };
+  mbutil = python3Packages.callPackage ./pkgs/mbutil { inherit (sources) mbutil; };
 }
