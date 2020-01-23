ba27eb8c5b22f241e8b815d8d3395c2331587a50
Nikolay Korotkiy <sikmir@gmail.com>
2020-01-23 22:01:14 +0300

    Move imgp to nixpkgs

--- default.nix
+++ default.nix
@@ -69,9 +69,6 @@ in rec {
   gt4gd = python3Packages.callPackage ./pkgs/gt4gd {
     inherit (sources) google-translate-for-goldendict;
   };
-  imgp = python3Packages.callPackage ./pkgs/imgp {
-    inherit (sources) imgp;
-  };
   ish = callPackage ./pkgs/ish { };
   libshell = callPackage ./pkgs/libshell {
     inherit (sources) libshell;
