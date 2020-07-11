1c3ccc6a8527777a0b64021e515d4cc2db91bfb1
Nikolay Korotkiy <sikmir@gmail.com>
2020-07-12 02:44:18 +0300

    gpxelevations: fix tests

--- pkgs/development/python-modules/gpxelevations/default.nix
+++ pkgs/development/python-modules/gpxelevations/default.nix
@@ -1,14 +1,23 @@
-{ lib, python3Packages, sources }:
+{ lib, python3Packages, fetchurl, sources }:
 let
   pname = "gpxelevations";
   date = lib.substring 0 10 sources.gpxelevations.date;
   version = "unstable-" + date;
+
+  testdata = import ./testdata.nix { inherit fetchurl; };
 in
 python3Packages.buildPythonApplication {
   inherit pname version;
   src = sources.gpxelevations;
 
   propagatedBuildInputs = with python3Packages; [ requests gpxpy ];
+
+  postPatch = ''
+    mkdir -p tmp_home/.cache/srtm
+    ${lib.concatMapStringsSep "\n" (hgt: ''
+      cp ${hgt} tmp_home/.cache/srtm/${hgt.name}
+    '') testdata}
+  '';
 
   checkPhase = ''
     HOME=tmp_home ${python3Packages.python.interpreter} -m unittest test
