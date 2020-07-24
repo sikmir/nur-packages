5340bc83e4edda25b69e5e735e90c807030854e2
Nikolay Korotkiy <sikmir@gmail.com>
2020-07-24 11:20:02 +0300

    Add gpxsee-bin

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -14,6 +14,7 @@ lib.makeScope newScope (
     gpxsee = libsForQt5.callPackage ./applications/gpxsee {
       inherit sources;
     };
+    gpxsee-bin = callPackage ./applications/gpxsee/bin.nix { };
     keeweb-bin = callPackage ./applications/keeweb/bin.nix { };
     librewolf = callPackage ./applications/librewolf { };
     macpass-bin = callPackage ./applications/macpass/bin.nix { };
