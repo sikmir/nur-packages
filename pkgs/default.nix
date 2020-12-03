cf7766cabfe40b91af1ac43870aaac1c22c45fbd
Nikolay Korotkiy <sikmir@gmail.com>
2020-12-03 21:22:09 +0300

    Add maperitive

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -28,6 +28,7 @@ lib.makeScope newScope (
     klogg-bin = callPackage ./applications/misc/klogg/bin.nix { };
     librewolf = callPackage ./applications/networking/librewolf { };
     macpass-bin = callPackage ./applications/macpass/bin.nix { };
+    maperitive-bin = callPackage ./applications/gis/maperitive/bin.nix { };
     mapsoft = callPackage ./applications/gis/mapsoft { };
     mapsoft2 = callPackage ./applications/gis/mapsoft/2.nix { };
     nnn-plugins = callPackage ./applications/nnn-plugins { };
