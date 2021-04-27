bed1ff900d50e9a50e61356fb2217580e639fef7
Nikolay Korotkiy <sikmir@gmail.com>
2021-04-27 13:01:14 +0300

    Add sthkd

---
+++ pkgs/suckless/sthkd/default.nix
@@ -0,0 +1,23 @@
+{ lib, stdenv, fetchFromGitHub }:
+
+stdenv.mkDerivation rec {
+  pname = "sthkd";
+  version = "2020-11-15";
+
+  src = fetchFromGitHub {
+    owner = "jeremybobbin";
+    repo = pname;
+    rev = "2cb198a8e0bc46b9e88c4a7b0f533b35d197a8f0";
+    hash = "sha256-P7RWdsxYv/P7K+BfvzkOzCCSMppSxacKVj19MPgeV7I=";
+  };
+
+  installFlags = [ "PREFIX=$(out)" ];
+
+  meta = with lib; {
+    description = "Simple Terminal Hotkey Daemon";
+    inherit (src.meta) homepage;
+    license = licenses.isc;
+    platforms = platforms.linux;
+    maintainers = [ maintainers.sikmir ];
+  };
+}
