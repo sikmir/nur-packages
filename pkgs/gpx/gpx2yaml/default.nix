8a5af4fa2f640e3256486617e1c157a708ddc700
Nikolay Korotkiy <sikmir@gmail.com>
2021-08-19 23:45:50 +0300

    gpx2yaml: use sr.ht repo

--- pkgs/gpx/gpx2yaml/default.nix
+++ pkgs/gpx/gpx2yaml/default.nix
@@ -1,12 +1,13 @@
-{ lib, stdenv, fetchgit }:
+{ lib, stdenv, fetchFromSourcehut }:
 
 stdenv.mkDerivation rec {
   pname = "gpx2yaml";
   version = "2021-08-19";
 
-  src = fetchgit {
-    url = "git://git.sikmir.ru/${pname}";
-    rev = "bc8f175d022d68e7712535a29cc2c08d9c79254e";
+  src = fetchFromSourcehut {
+    owner = "~sikmir";
+    repo = pname;
+    rev = "d4a102ab079edf239a095bdd3564be3cac193971";
     sha256 = "sha256-82RYdC1tUYwYZELTHOC+Llz+KcdLgyipB9wEWekDRww=";
   };
 
@@ -14,7 +15,7 @@ stdenv.mkDerivation rec {
 
   meta = with lib; {
     description = "GPX to YAML converter";
-    homepage = "https://git.sikmir.ru/gpx2yaml/file/README.md.html";
+    homepage = "https://git.sikmir.ru/gpx2yaml";
     license = licenses.isc;
     maintainers = [ maintainers.sikmir ];
     platforms = platforms.unix;
