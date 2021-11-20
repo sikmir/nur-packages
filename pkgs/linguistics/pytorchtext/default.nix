c97a1ca63ff3e2fa26ad5de8b0e340023cdb533a
Nikolay Korotkiy <sikmir@gmail.com>
2021-11-20 12:26:09 +0300

    pytorchtext: broken on darwin

--- pkgs/linguistics/pytorchtext/default.nix
+++ pkgs/linguistics/pytorchtext/default.nix
@@ -40,5 +40,6 @@ python3Packages.buildPythonPackage rec {
     inherit (src.meta) homepage;
     license = licenses.bsd3;
     maintainers = [ maintainers.sikmir ];
+    broken = stdenv.isDarwin; # https://github.com/NixOS/nixpkgs/issues/94241
   };
 }
