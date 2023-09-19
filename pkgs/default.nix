ba95c2955ceb02fd65aeff3ccd45d182cacdf8b8
Nikolay Korotkiy <sikmir@disroot.org>
2023-09-19 22:58:03 +0400

    Add gcgn-converter

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -439,6 +439,7 @@ lib.makeScope newScope (
     ### OSM
 
     cykhash = callPackage ./osm/cykhash { };
+    gcgn-converter = callPackage ./osm/gcgn-converter { };
     imposm = callPackage ./osm/imposm { };
     map-machine = callPackage ./osm/map-machine { };
     map-stylizer = callPackage ./osm/map-stylizer { };
