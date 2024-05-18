e242f582c6b3f1d8d553ddb158667855f250e905
Nikolay Korotkiy <sikmir@disroot.org>
2024-05-18 14:51:38 +0400

    Up

--- pkgs/data/dicts/tatoeba/default.nix
+++ pkgs/data/dicts/tatoeba/default.nix
@@ -36,7 +36,7 @@ let
 in
 stdenvNoCC.mkDerivation rec {
   pname = "tatoeba";
-  version = "2024-05-11";
+  version = "2024-05-18";
 
   srcs = lib.mapAttrsToList (name: spec: fetchurl spec) tatoeba;
 
