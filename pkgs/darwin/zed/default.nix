1d6e4ef2a1b166a58b3b4bf5e5db0506826fe80e
Nikolay Korotkiy <sikmir@disroot.org>
2025-08-20 19:46:10 +0400

    Up

--- pkgs/darwin/zed/default.nix
+++ pkgs/darwin/zed/default.nix
@@ -7,11 +7,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "zed-preview";
-  version = "0.200.1-pre";
+  version = "0.200.4-pre";
 
   src = fetchurl {
     url = "https://zed.dev/api/releases/stable/${finalAttrs.version}/Zed-x86_64.dmg";
-    hash = "sha256-PX9UK8z1aBreQpLXduSILe/8Hw4cQ2tPCzw2gw7+Fo0=";
+    hash = "sha256-xYEoeGxWTiyoRTD4YyRcbDcJYTdT+5II2bhzVIkSXPk=";
   };
 
   sourceRoot = ".";
