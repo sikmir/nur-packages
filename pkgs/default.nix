a57bf5abeab810736e772fd66e71246ade66e896
Nikolay Korotkiy <sikmir@disroot.org>
2025-08-30 12:03:14 +0400

    Add rodnik

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -399,6 +399,7 @@ lib.makeScope newScope (
     repolocli = callPackage ./misc/repolocli { };
     rhttp = callPackage ./misc/rhttp { };
     riffraff = callPackage ./misc/riffraff { };
+    rodnik = callPackage ./misc/rodnik { };
     rst2txt = callPackage ./misc/rst2txt { };
     sdorfehs = callPackage ./misc/sdorfehs { };
     serverpp = callPackage ./misc/serverpp { };
