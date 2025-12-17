7cb70e21b72f3151ba6a3cf8c4231fd7c02a54b4
Nikolay Korotkiy <sikmir@disroot.org>
2025-12-17 21:16:36 +0400

    Up

--- pkgs/by-name/sa/sasplanet/package.nix
+++ pkgs/by-name/sa/sasplanet/package.nix
@@ -4,6 +4,9 @@
   fetchfromgh,
   fetchgdrive,
   fetchgit,
+  fetchurl,
+  copyDesktopItems,
+  makeDesktopItem,
   unzip,
   wineWow64Packages,
   makeWrapper,
@@ -21,6 +24,10 @@ let
     hash = "sha256-6ZF4PsEFEGYt85umWJ/ToBW3JdeKEF4n6uU73hU8oLs=";
     name = "Extremum.zip";
   };
+  icon = fetchurl {
+    url = "https://raw.githubusercontent.com/sasgis/sas.planet.src/8f8c9aedd9a641feec419a5609474d3a75cc24c9/Resources/MainIcon/icon64.png";
+    hash = "sha256-KQR5fGrF6cLBDeIEVmU+YPQIdzFn43NrPLbTqGAKhp0=";
+  };
 in
 stdenv.mkDerivation (finalAttrs: {
   pname = "sasplanet";
@@ -37,26 +44,45 @@ stdenv.mkDerivation (finalAttrs: {
   sourceRoot = ".";
 
   nativeBuildInputs = [
-    unzip
+    copyDesktopItems
     makeWrapper
+    unzip
+  ];
+
+  desktopItems = [
+    (makeDesktopItem {
+      name = "sasplanet";
+      type = "Application";
+      desktopName = "SAS.Planet";
+      icon = "sasplanet";
+      exec = "sasplanet";
+      terminal = false;
+      categories = [
+        "Science"
+        "Maps"
+        "Geography"
+      ];
+    })
   ];
 
   # Post install regedit:
   # * increase font size: https://askubuntu.com/a/1313810
   # * dark theme: https://gist.github.com/Zeinok/ceaf6ff204792dde0ae31e0199d89398
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/opt/sasplanet
     cp -r . $out/opt/sasplanet
+    install -Dm644 ${icon} $out/share/icons/hicolor/64x64/apps/sasplanet.png
 
     makeWrapper ${wineWow64Packages.stable}/bin/wine $out/bin/sasplanet \
       --run "[ -d \$HOME/.sasplanet ] || { cp -r $out/opt/sasplanet \$HOME/.sasplanet && chmod -R +w \$HOME/.sasplanet; }" \
       --add-flags "\$HOME/.sasplanet/SASPlanet.exe"
-  ''
-  + lib.optionalString withMaps ''
-    cp -r ${maps}/* $out/opt/sasplanet/Maps/sas.maps
-  ''
-  + lib.optionalString withExtremum ''
-    unzip ${extremum} -d $out/opt/sasplanet/Maps/sas.maps
+
+    ${lib.optionalString withMaps "cp -r ${maps}/* $out/opt/sasplanet/Maps/sas.maps"}
+    ${lib.optionalString withExtremum "unzip ${extremum} -d $out/opt/sasplanet/Maps/sas.maps"}
+
+    runHook postInstall
   '';
 
   preferLocalBuild = true;
@@ -69,6 +95,7 @@ stdenv.mkDerivation (finalAttrs: {
     license = lib.licenses.gpl3Plus;
     maintainers = [ lib.maintainers.sikmir ];
     platforms = lib.platforms.all;
+    mainProgram = "sasplanet";
     skip.ci = true;
   };
 })
